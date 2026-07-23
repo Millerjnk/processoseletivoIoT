64a1f1f9255108e3f39fd2bcfb71f6f36ad0543d
Guilherme Miller <guimigc@protonmail.com>
2026-07-23 12:45:24 -0300

    src: main.py: Retaforando acionamento do botão de reset da esteira. O botão agora não é tratado por uma ISR, ela é tratada pela função turno_reset que é chamada no loop principal e verifica se o botão foi solto ao invés de pressionado, sendo ativado na sua borda de subida ao invés da de descida

--- src/main.py
+++ src/main.py
@@ -20,7 +20,8 @@ obj_no_sensor = False
 tempo_estouro = False
 contador_itens = 0
 botao_foi_pressionado = False
-tempo_atual = tempo_passado = 0
+tempo_atual = 0
+tempo_passado = 0
 
 print("Contador de Producao Inicializado\n")
 
@@ -33,41 +34,48 @@ def read_lux():
 
     return lux
 
-
 def timer_acabado(timer_obj):
     global tempo_estouro
 
-    if(obj_no_sensor):
+    if obj_no_sensor:
         tempo_estouro = True
 
-def ISR_botao(botao):
-    global botao_foi_pressionado, tempo_atual, tempo_passado  
+def turno_reset(pino):
+    global contador_itens, tempo_atual, tempo_passado, obj_no_sensor, tempo_estouro, botao_foi_pressionado
 
-    if botao_foi_pressionado == False:
-        tempo_atual = time.ticks_ms()
-        if (time.ticks_diff(tempo_atual, tempo_passado)) > 500:
+    if pino.value() == 0:
+        if not botao_foi_pressionado and time.ticks_diff(tempo_atual, tempo_passado) > 300:
             botao_foi_pressionado = True
             tempo_passado = tempo_atual
 
-botao.irq(trigger=Pin.IRQ_FALLING, handler=ISR_botao)
+    elif pino.value() == 1:
+        if botao_foi_pressionado:
+            botao_foi_pressionado = False
+            contador_itens = 0
+            obj_no_sensor = False
+            tempo_passado = tempo_atual
+            tempo_estouro = False
+            timer.deinit() 
+            print("Turno resetado com sucesso. Contadores zerados.")
 
 while True:
+    tempo_atual = time.ticks_ms()
 
     lux = read_lux()
 
     if lux < LUX_ESTEIRA_BLOQUEADA:
         if not obj_no_sensor:
-            # Inicia um timer em modo One Shot para acabar em LIMAR_PARADA milissegundos
+            # Inicia um timer em modo One Shot para acabar em LIMIAR_PARADA milissegundos
             # caso o objeto não avance na esteira
             timer.init(period=LIMIAR_PARADA,            
-                mode=Timer.ONE_SHOT,         
-                callback=timer_acabado)     
+                       mode=Timer.ONE_SHOT,         
+                       callback=timer_acabado)  
             obj_no_sensor = True
 
         if tempo_estouro:
             print("Micro-parada detectada!")
             tempo_estouro = False
-        
+            
     if lux >= LUX_ESTEIRA_LIVRE and obj_no_sensor:
         # Desinicializa o timer se o objeto passar pelo sensor antes do tempo
         timer.deinit()
@@ -76,7 +84,7 @@ while True:
         contador_itens += 1
         print(f"Peca detectada! Total: {contador_itens}")
 
-    if botao_foi_pressionado:
-        print("Turno resetado com sucesso. Contadores zerados.")
-        contador_itens = 0
-        botao_foi_pressionado = False
+    turno_reset(botao)
+
+    # Pequeno delay para aliviar o micro
+    time.sleep_ms(10)
