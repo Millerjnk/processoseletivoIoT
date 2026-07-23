c11d51b5f0cacc616b0e63670050911230c96056
Guilherme Miller <guimigc@protonmail.com>
2026-07-23 12:19:36 -0300

    src: main.py: Reestruturação do código com novas constantes e comentários. Sem mudanças funcionais

--- src/main.py
+++ src/main.py
@@ -2,30 +2,46 @@ import time
 from machine import Pin, ADC, Timer
 from math import pow
 
-#Inicializando variáveis do sistema
-
+# Inicialização de pinos e timer
 ldr_sensor = ADC(Pin(34))
-botao = Pin(14, Pin.IN, Pin.PULL_UP)
-# Configura a atenuação para ler a faixa completa de 0-3.3V
-# ATTN_11DB é a configuração padrão para isso
 ldr_sensor.atten(ADC.ATTN_11DB)
+botao = Pin(14, Pin.IN, Pin.PULL_UP)
+timer = Timer(0)
+
+# Inicialização de constantes
 GAMMA = 0.7
 RL10 = 50
-timer = Timer(0)
+LIMIAR_PARADA = 5000 # em ms
+LUX_ESTEIRA_LIVRE = 500
+LUX_ESTEIRA_BLOQUEADA = 100
+
+# Inicialização de variáveis globais
 obj_no_sensor = False
 tempo_estouro = False
 contador_itens = 0
 botao_foi_pressionado = False
 tempo_atual = tempo_passado = 0
+
 print("Contador de Producao Inicializado\n")
+
+def read_lux():
+    # Fórmula para extração do valor do Lux retirada da documentação oficial do sensor LDR
+    valor_lido = ldr_sensor.read()
+    tensao = valor_lido / 4096 * 5
+    resistencia = 2000 * tensao / (1 - tensao / 5)
+    lux = pow(RL10 * 1e3 * pow(10, GAMMA) / resistencia, (1 / GAMMA))
+
+    return lux
+
 
 def timer_acabado(timer_obj):
     global tempo_estouro
+
     if(obj_no_sensor):
         tempo_estouro = True
 
 def ISR_botao(botao):
-    global botao_foi_pressionado, tempo_atual, tempo_passado  # Avisa que vamos usar a variável global
+    global botao_foi_pressionado, tempo_atual, tempo_passado  
 
     if botao_foi_pressionado == False:
         tempo_atual = time.ticks_ms()
@@ -36,23 +52,24 @@ def ISR_botao(botao):
 botao.irq(trigger=Pin.IRQ_FALLING, handler=ISR_botao)
 
 while True:
-    valor_lido = ldr_sensor.read()
-    tensao = valor_lido / 4096 * 5
-    resistencia = 2000 * tensao / (1 - tensao / 5)
-    lux = pow(RL10 * 1e3 * pow(10, GAMMA) / resistencia, (1 / GAMMA))
-    #print(lux)
 
-    if lux < 100:
+    lux = read_lux()
+
+    if lux < LUX_ESTEIRA_BLOQUEADA:
         if not obj_no_sensor:
-            timer.init(period=3000,            # Período em milissegundos (200ms = 5 vezes por segundo)
-                mode=Timer.ONE_SHOT,         # Modo: PERIODIC (repete) ou ONE_SHOT (uma vez)
-                callback=timer_acabado)      # A função a ser chamada quando o timer estourar
+            # Inicia um timer em modo One Shot para acabar em LIMAR_PARADA milissegundos
+            # caso o objeto não avance na esteira
+            timer.init(period=LIMIAR_PARADA,            
+                mode=Timer.ONE_SHOT,         
+                callback=timer_acabado)     
             obj_no_sensor = True
+
         if tempo_estouro:
             print("Micro-parada detectada!")
             tempo_estouro = False
         
-    if lux >= 500 and obj_no_sensor:
+    if lux >= LUX_ESTEIRA_LIVRE and obj_no_sensor:
+        # Desinicializa o timer se o objeto passar pelo sensor antes do tempo
         timer.deinit()
         obj_no_sensor = False
         tempo_estouro = False
